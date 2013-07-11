f8a06aa8dd5072e31f0f231dfd826cb99fe044da
Yi Wen <ywen.github@gmail.com>
2013-07-11 11:51:01 -0500

    Switch it to Raisl 4

--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,19 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111128232157) do
+ActiveRecord::Schema.define(version: 20111128232157) do
 
-  create_table "clients", :force => true do |t|
+  create_table "clients", force: true do |t|
     t.string   "name"
     t.string   "company"
     t.string   "website"
@@ -19,7 +21,7 @@ ActiveRecord::Schema.define(:version => 20111128232157) do
     t.datetime "updated_at"
   end
 
-  create_table "proposal_sections", :force => true do |t|
+  create_table "proposal_sections", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "proposal_id"
@@ -27,7 +29,7 @@ ActiveRecord::Schema.define(:version => 20111128232157) do
     t.datetime "updated_at"
   end
 
-  create_table "proposals", :force => true do |t|
+  create_table "proposals", force: true do |t|
     t.string   "name"
     t.string   "user_name"
     t.date     "send_date"
