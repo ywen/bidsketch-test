54d3ae9c7889141f7ad6361acc141b4fc6400b60
Yi Wen <ywen.github@gmail.com>
2013-07-12 09:39:14 -0500

    Add README

--- app/assets/javascripts/body.js.coffee
+++ app/assets/javascripts/body.js.coffee
@@ -1,2 +1,4 @@
 $(document).ready ->
-  console.log("aaaa")
+  # $.getJSON("/static_templates", (data, textStatus, jqXHR) ->
+  #   $("body").load(data["template"])
+  # )
--- app/controllers/proposal_viewer_controller.rb
+++ app/controllers/proposal_viewer_controller.rb
@@ -2,6 +2,8 @@ class ProposalViewerController < ApplicationController
   layout false
 
   def show
+    template = "#{Rails.root}/public/proposal-template/index.html"
+    render file: template
   end
 
 end
